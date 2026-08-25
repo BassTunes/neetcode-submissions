b909907b1e0c127305052b4daf24553219903a88
Koh Loke Peen Caleb <lokepeen@gmail.com>
2026-08-25 14:56:12 +0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (auto x : strs) {
+            string s = x;
+            sort(s.begin(), s.end());
+            mp[s].push_back(x);
+        }
+        vector<vector<string>> st;
+
+        for (auto &it : mp) {
+            st.push_back(it.second);
+        }
+        return st;
+  }
+};
