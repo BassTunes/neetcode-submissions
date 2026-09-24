0d90553bee63f3431861902fc145cf0a5eb466d4
Koh Loke Peen Caleb <lokepeen@gmail.com>
2026-09-24 13:14:46 +0800

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+       if (nums.size()==0)
+            return 0;
+        unsigned int count = 1, max_count = 1;
+        std::set<int> s(nums.begin(), nums.end());
+
+        unsigned int size = s.size();
+        auto it = s.begin();
+        unsigned int loop = 1;
+        while (1) {
+            if (s.contains(*it+1)) {
+                max_count=std::max(max_count,++count);
+                ++it;
+            }
+            else {
+                count=1;
+                ++it;
+            }
+
+            if (++loop >= size)
+                break;
+        }
+
+        return max_count;
+    }
+};
